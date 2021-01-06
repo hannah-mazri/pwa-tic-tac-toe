909bdf4e8c594973dbcde300a7cd458e4767f3f0
hannah <hannahnajwa@gmail.com>
2021-01-06 12:40:56 +0800

    Render 9 square components in board component and add methods to handle clicks

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  squares: any;
+  status = 'Next player: X';
+  xIsNext = true;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): any {
+    this.renderSquare();
   }
 
+  renderSquare(): any {
+    this.squares = Array(9).fill(null);
+  }
+
+  handleClick(i): any {
+    const squares = this.squares.slice();
+    squares[i] = this.xIsNext ? 'X' : 'O';
+    this.squares = squares;
+    this.xIsNext = !this.xIsNext;
+
+    this.status = `Next player: ${ this.xIsNext ? 'X' : 'O' }`;
+  }
 }
